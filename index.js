029749e67368518f39195d1213448b10326868ea
ZeroZeroZerooZeroo <misha.solodownick@yandex.ru>
2025-05-24 01:18:18 +0700

    Creating a DATABASE connection

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const ExpressServer = require('./expressServer');
 
 const launchServer = async () => {
   try {
+    const port = process.env.PORT;
     this.expressServer = new ExpressServer(config.URL_PORT, config.OPENAPI_YAML);
     this.expressServer.launch();
     logger.info('Express server running');
